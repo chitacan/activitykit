3f284c59a4a5aae20d9bedfd925d786c20fca60e
kyung yeol kim <chitacan@gmail.com>
2014-06-25 00:29:07 +0900

    create a function to parse taskInfo.

--- parser/taskRecord.js
+++ parser/taskRecord.js
@@ -16,6 +16,20 @@ module.exports = TaskRecordParser;
 util.inherits(TaskRecordParser, Transform);
 
 TaskRecordParser.prototype._transform = function(chunk, encoding, cb) {
+
+  function parseTaskInfo(taskInfoArray) {
+    // see com.android.server.am.TaskRecord.toString()
+    var result = {}
+    result['hash']     = taskInfoArray[0]
+    result['taskId']   = taskInfoArray[1].substring(1);
+    // this can be either 'A=' or 'I='
+    result['affinity'] = taskInfoArray[2].substring(2);
+    result['user']     = taskInfoArray[3].substring(2);
+    result['history']  = taskInfoArray[4].substring(2);
+
+    return result;
+  }
+
   if (chunk.length === 0) {
     cb();
     return;
@@ -24,19 +38,13 @@ TaskRecordParser.prototype._transform = function(chunk, encoding, cb) {
   var line = chunk.toString('utf8').trim();
   var idx  = line.indexOf(PREFIX_TASK);
   if (idx >= 0) {
-    var r = line.substring(idx + PREFIX_TASK.length, line.length -1).split(' ');
-    var obj = {};
-    obj['hash']     = r[0]
-    obj['taskId']   = r[1].substring(1);
-    // this can be either 'A=' or 'I='
-    obj['affinity'] = r[2].substring(2);
-    obj['user']     = r[3].substring(2);
-    obj['history']  = r[4].substring(2);
+    var taskInfoArray = line.substring(idx + PREFIX_TASK.length, line.length -1).split(' ');
+    var taskInfo      = parseTaskInfo(taskInfoArray);
 
     if (idx == 2) {
-      this.push(JSON.stringify(obj) + EOL);
+      this.push(JSON.stringify(taskInfo) + EOL);
     } else {
-      this.push(line.substring(0, idx) + JSON.stringify(obj) + EOL);
+      this.push(line.substring(0, idx) + JSON.stringify(taskInfo) + EOL);
     }
   } else {
     this.push(chunk);
