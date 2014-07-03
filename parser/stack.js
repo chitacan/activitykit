8bf815bd4a1974056229452084494c7adb84fecc
kyung yeol kim <chitacan@gmail.com>
2014-07-03 13:24:49 +0900

    create flush* functions.

--- parser/stack.js
+++ parser/stack.js
@@ -29,6 +29,36 @@ module.exports = Stack;
 
 P.yeild(Stack);
 
+Stack.prototype._flushStack = function(name) {
+  this._flushTask(name);
+  if (!_.isEmpty(this._stack))
+    this._result.stack.children.push(this._stack);
+
+  this._stack = {
+    name: name,
+    type: 'stack',
+    children: []
+  }
+}
+
+Stack.prototype._flushTask = function(name) {
+  if (!_.isEmpty(this._task))
+    this._stack.children.push(this._task);
+
+  this._task = {
+    type: 'task',
+    name: name,
+    intent: {},
+    data: {},
+    children: []
+  }
+}
+
+Stack.prototype._flushRecent = function() {
+  if (!_.isEmpty(this._recent))
+    this._result.recent.push(this._recent);
+}
+
 Stack.prototype._transform = function(chunk, encoding, done) {
 
   if (chunk.length === 0) {
@@ -46,26 +76,11 @@ Stack.prototype._transform = function(chunk, encoding, done) {
 
   // 'Stack #0:'
   if (isStack) {
-    if (!_.isEmpty(this._stack))
-      this._result.stack.children.push(this._stack);
-
-    this._stack = {
-      name: line,
-      type: 'stack',
-      children: []
-    }
+    this._flushStack(line);
   }
   // 'Task id #2'
   else if (isTask) {
-    if (!_.isEmpty(this._task))
-      this._stack.children.push(this._task);
-
-    this._task = {
-      name: line,
-      intent: {},
-      data: {},
-      children: []
-    }
+    this._flushTask(line);
     this._in = 'task';
   }
   // 'mFocusedActivity:'
@@ -99,8 +114,7 @@ Stack.prototype._transform = function(chunk, encoding, done) {
       }
     }
     else {
-      if (!_.isEmpty(this._recent))
-        this._result.recent.push(this._recent);
+      this._flushRecent();
       this._recent = JSON.parse(line);
     }
   }
@@ -110,14 +124,8 @@ Stack.prototype._transform = function(chunk, encoding, done) {
 
 Stack.prototype._flush = function(done) {
   // assemble remainder
-  if (!_.isEmpty(this._task))
-    this._stack.children.push(this._task);
-
-  if (!_.isEmpty(this._stack))
-    this._result.stack.children.push(this._stack);
-
-  if (!_.isEmpty(this._recent))
-    this._result.recent.push(this._recent);
+  this._flushStack();
+  this._flushRecent();
 
   this.push(JSON.stringify(this._result, null, 4));
   done();
